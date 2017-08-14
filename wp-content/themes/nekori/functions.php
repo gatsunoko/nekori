37f169dde214ec13d160db87fc8e910f309c43ab
gatsunoko <gatsunoko@yahoo.co.jp>
2017-08-14 22:17:00 +0900

    人気記事ランキング実装

--- wp-content/themes/nekori/functions.php
+++ wp-content/themes/nekori/functions.php
@@ -18,3 +18,46 @@ function the_slug() {
 }
 
 add_theme_support('post-thumbnails');
+
+//---------------人気記事一覧を表示-----------------------
+function most_viewed_posts() {
+  ob_start(); // Buffer output
+?>
+<ul class="ranking_articles">
+  <?php
+    // 以下のWP_Queryで表示回数上位５件の記事を取得
+    $query = new WP_Query(array(
+      'meta_key' => 'views',
+      'orderby' => 'meta_value_num',
+      'order' => 'DESC',
+      'posts_per_page' => 5
+    ));
+    
+    // 以下のwhileループで上記で取得した人気上位５件の記事をhtml含めて出力
+    while ($query->have_posts()) : $query->the_post();
+  ?>
+  <li class="article">
+    <a href="<?php the_permalink(); ?>">
+      <div class="thumbnail" style="background-image: url('<?php echo get_the_post_thumbnail_url( $post_id, 'large' ); ?>')">
+      </div>
+      <div class="text_area">
+          <p class="title"><?php the_title(); ?></p>
+          <p class="contributor"><?php the_author(); ?></p>
+      </div>
+    </a>
+  </li>
+  <?php
+    endwhile;
+  ?>
+</ul>
+<?php
+$output = ob_get_clean(); // clear buffer
+return $output;
+}
+ 
+// most_viewd_posts()のショートコードを作成
+add_shortcode('most_viewed', 'most_viewed_posts');
+ 
+// ショートコードをテキストウィジェットで使用できるようにするためのフィルタ
+add_filter('widget_text', 'do_shortcode');
+//---------------人気記事一覧を表示　ここまで-----------------------
